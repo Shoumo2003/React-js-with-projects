73e2c1a3616f5b1d6b7379cbd5e0b0d7c1bcb555
Shoumo Mondal <shoumomondal@gmail.com>
2024-08-29 01:47:46 +0530

    29/08/2024

---
+++ 01_viteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './Chai'
+
+function App(){
+    return (
+        <>
+        <Chai/>
+        <h1>Chai aur Code with vite</h1>
+        <p>test para</p>
+        </>
+    )
+}
+
+export default App;
